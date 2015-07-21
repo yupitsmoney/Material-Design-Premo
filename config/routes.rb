41a647a42309f60b43065894dddae9a4f29375d3
Money <money@moneysingh.com>
2015-07-21 14:01:13 -0700

    changed route to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'main#about'
+  root 'main#index'
   # root :to => 'games#level_one'
 
   # You can have the root of your site routed with "root"
